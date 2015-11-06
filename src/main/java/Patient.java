41922cde23d9d4abd99f4c2172423dadba4cf578
Zinan Lin <zinan.lin@sphic.org.cn>
2015-11-06 15:24:40 +0800

    Init, working solution with rest API and Hibernate

---
+++ src/main/java/Patient.java
@@ -0,0 +1,62 @@
+package main.java;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="patient")
+public class Patient {
+
+	@Id
+	@Column(name="id")
+    private int id;
+	@Column(name="name")
+    private String name;
+	@Column(name="address")
+    private String address;
+	@Column(name="phone")
+    private String phone;
+
+    public String getName() {
+		return name;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Patient() {		
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+    public Patient(int id, String name, String address, String phone) {		
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+	}
+
+	public long getId() {
+        return id;
+    }
+}
---
+++ src/main/java/PatientController.java
@@ -0,0 +1,29 @@
+package main.java;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patient")
+public class PatientController {
+	private SessionFactory sf = HibernateUtil.getSessionFactory();
+	
+	
+    public PatientController() {
+	}
+
+	@RequestMapping("/info")
+    public Patient Info(@RequestParam(value="id", defaultValue="1") int id) {
+		Session session = sf.openSession();
+		Patient patient = (Patient)session.get(Patient.class, id);
+		session.close();
+		
+		if (patient == null) {
+			return new Patient();			
+		}
+		
+        return patient;
+    }
+}
