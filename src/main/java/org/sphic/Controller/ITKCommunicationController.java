0b474890575f4975b65bba341b528259530f7852
Zinan Lin <simonlininter@yahoo.com>
2015-11-19 14:39:33 +0800

    Reformatted the api for sending function calls with seeds

--- src/main/java/org/sphic/Controller/ITKCommunicationController.java
+++ src/main/java/org/sphic/Controller/ITKCommunicationController.java
@@ -9,9 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.HashMap;
+import java.util.List;
 
 
 @RestController
@@ -26,18 +30,54 @@ public class ITKCommunicationController {
 		this.messageQueue = messageQueue;
 	}
 
-	@RequestMapping("/call")
-	public DeferredResult<String> Info(@RequestParam(defaultValue = "" ) MultiValueMap params) throws JsonProcessingException {
+//	@RequestMapping(value = "/call", method = RequestMethod.GET)
+//	public DeferredResult<String> Info(@RequestParam(value = "func" , defaultValue = "" ) String func) throws JsonProcessingException {
+//		messageQueue.Send(new ObjectMapper().writeValueAsString(func));
+//		DeferredResult<String> result = new DeferredResult<String>();
+//		subscriber =
+//		new Subscriber(){
+//			@Override
+//			public void Callback(String message){
+//				result.setResult(message);
+//				PubSub.Unsubscribe("rabbitmq", subscriber);
+//			}
+//		};
+//		PubSub.Subscribe("rabbitmq", subscriber);
+//		return result;
+//	}
+
+	@RequestMapping(value = "/call", method = RequestMethod.GET)
+	public DeferredResult<String> Info(@RequestParam MultiValueMap func) throws JsonProcessingException {
+		messageQueue.Send(new ObjectMapper().writeValueAsString(func));
+		DeferredResult<String> result = new DeferredResult<String>();
+		subscriber =
+				new Subscriber(){
+					@Override
+					public void Callback(String message){
+						result.setResult(message);
+						PubSub.Unsubscribe("rabbitmq", subscriber);
+					}
+				};
+		PubSub.Subscribe("rabbitmq", subscriber);
+		return result;
+	}
+
+	// $.ajax({url: 'http://localhost:8080/itk/call?func=render', type:'POST', data:  {seeds : ["(1,1,1)","(2,2,2)","(3,3,3)"]}, success:function(ret){console.log(ret)}})
+	@RequestMapping(value = "/call", method = RequestMethod.POST)
+	public DeferredResult<String> Info(@RequestParam(value = "func" , defaultValue = "" ) String func, @RequestParam(value = "seeds[]" , defaultValue = "" ) String[] seeds) throws JsonProcessingException {
+		HashMap params = new HashMap<String, List<String>>();
+		params.put("func", func);
+		params.put("seeds", seeds);
 		messageQueue.Send(new ObjectMapper().writeValueAsString(params));
 		DeferredResult<String> result = new DeferredResult<String>();
 		subscriber =
-		new Subscriber(){
-			@Override
-			public void Callback(String message){
-				result.setResult(message);
-				PubSub.Unsubscribe("rabbitmq", subscriber);
-			}
-		};
+				new Subscriber(){
+					@Override
+					public void Callback(String message){
+						result.setResult(message);
+						PubSub.Unsubscribe("rabbitmq", subscriber);
+					}
+				};
 		PubSub.Subscribe("rabbitmq", subscriber);
 		return result;
 	}
